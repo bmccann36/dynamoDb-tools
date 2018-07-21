19f634a76d5c169f87d5b5444c0aedb55557be55
Brian McCann <bmccann36@gmail.com>
2018-07-21 09:32:41 -0400

    Update sampleArguments.js

--- sampleArguments.js
+++ sampleArguments.js
@@ -2,7 +2,7 @@
 // update
 
   var params = {
-    TableName: `vendorzones-microservice-steve-VendorZoneDynamoTable-69GKUEQ2U3JY`,
+    TableName: `DynamoTable-69GKUEQ2U3JY`,
     Key: { id: '50d51efb-c5c1-414a-9925-9136c37aeab2' },
     UpdateExpression: 'set #em = :em',
     ExpressionAttributeNames: { '#em': 'endMile' },
